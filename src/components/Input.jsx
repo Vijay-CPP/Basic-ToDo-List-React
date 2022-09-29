a490166fdda21f7f6e42e344cf79007a77141254
T Vijay Kumar <yourstrulyvijay12@gmail.com>
2022-09-29 13:21:39 +0530

    initial commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,8 @@
+export default function Input(props) {
+  return (
+    <div className="Input">
+      <input type="text" placeholder="Enter Task Here..." className="input-field" />
+      <button onClick={props.addItem} className="btn btn-primary btn-sm">Add</button>
+    </div>
+  );
+}
